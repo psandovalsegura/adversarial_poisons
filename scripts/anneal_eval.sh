3cadc82e0320739b43cfaca0775b9d0652d70d59
Pedro Sandoval <psandovalsegura@gmail.com>
2021-12-08 12:54:02 -0500

    minor changes to submission scripts for specific experiment

--- scripts/anneal_eval.sh
+++ scripts/anneal_eval.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=anneal
+#SBATCH --account=djacobs
+#SBATCH --job-name=anneal-2
 #SBATCH --time=1-12:00:00
 #SBATCH --partition=dpart
 #SBATCH --qos=high
 #SBATCH --ntasks=1
-#SBATCH --gres=gpu:p6000:4
+#SBATCH --gres=gpu:p6000:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=8G
 #SBATCH --mail-type=end          
@@ -26,18 +27,19 @@ pip3 install --upgrade pip
 pip3 install -r ${SCRIPT_DIR}/requirements.txt
 
 export POISON_DATASET_DIR='/vulcanscratch/psando/untrainable_datasets/adv_poisons/fresh_craft'
-export MODEL_NAME='ResNet50'
+export MODEL_NAME='ResNet18'
 export RECIPE='targeted'
-export ATTACKITER='50'
+export ATTACKITER='250'
+export EPS='2'
 
 # Craft poison
 python anneal.py --net $MODEL_NAME --dataset CIFAR10 --data_path /vulcanscratch/psando/cifar-10/ \
---recipe $RECIPE --eps 8 --budget 1.0 --save poison_dataset \
+--recipe $RECIPE --eps ${EPS} --budget 1.0 --save poison_dataset \
 --cifar_ckpt_dir /vulcanscratch/psando/cifar_model_ckpts/ --cifar_adv_ckpt_dir /vulcanscratch/psando/cifar_model_ckpts/adv \
---poison_path ${POISON_DATASET_DIR}/${RECIPE}_${MODEL_NAME}_iter=${ATTACKITER} \
+--poison_path ${POISON_DATASET_DIR}/${RECIPE}_${MODEL_NAME}_iter=${ATTACKITER}_eps=${EPS} \
 --attackoptim PGD --attackiter $ATTACKITER
 
 # Evaluate poison
-python poison_evaluation/main.py --model_name $MODEL_NAME --epochs 100 \
---poison_path ${POISON_DATASET_DIR}/${RECIPE}_${MODEL_NAME}_iter=${ATTACKITER} \
---cifar_path /vulcanscratch/psando/cifar-10 --disable_tqdm
+# python poison_evaluation/main.py --model_name $MODEL_NAME --epochs 100 \
+# --poison_path ${POISON_DATASET_DIR}/${RECIPE}_${MODEL_NAME}_name=${ATTACKNAME}_iter=${ATTACKITER} \
+# --cifar_path /vulcanscratch/psando/cifar-10 --disable_tqdm --workers 4 --lr 0.025 --use_wd --use_scheduler
--- scripts/anneal_eval_functional.sh
+++ scripts/anneal_eval_functional.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 #SBATCH --account=djacobs
-#SBATCH --job-name=anneal-functional-test
+#SBATCH --job-name=anneal-functional-C
 #SBATCH --time=1-12:00:00
 #SBATCH --partition=dpart
 #SBATCH --qos=high
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:p6000:1
-#-- SBATCH --cpus-per-task=4
-#-- SBATCH --mem-per-cpu=8G
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=8G
 #SBATCH --mail-type=end          
 #SBATCH --mail-type=fail         
 #SBATCH --mail-user=psando@umd.edu
 # -- SBATCH --dependency=afterok:
 
 set -x
+echo "anneal-functional-C"
 
 export WORK_DIR="/scratch0/slurm_${SLURM_JOBID}"
 export SCRIPT_DIR="/cfarhomes/psando/Documents/adversarial_poisons"
@@ -29,20 +30,20 @@ pip3 install -r ${SCRIPT_DIR}/requirements.txt
 export POISON_DATASET_DIR='/vulcanscratch/psando/untrainable_datasets/adv_poisons/fresh_craft'
 export MODEL_NAME='ResNet18'
 export RECIPE='functional'
-export ATTACKITER='1'
-export ATTACKNAME='stadv'
+export ATTACKITER='100'
+export ATTACKNAME='recoloradv'
 
 # Craft poison
-python anneal.py --net $MODEL_NAME --dataset CIFAR10 --data_path /vulcanscratch/psando/cifar-10/ \
---recipe $RECIPE --eps 8 --budget 1.0 --save poison_dataset \
---cifar_ckpt_dir /vulcanscratch/psando/cifar_model_ckpts/ --cifar_adv_ckpt_dir /vulcanscratch/psando/cifar_model_ckpts/adv \
---poison_path ${POISON_DATASET_DIR}/${RECIPE}_${MODEL_NAME}_name=${ATTACKNAME}_iter=${ATTACKITER} \
---attackname ${ATTACKNAME} --attackiter $ATTACKITER --init zero --pretrained
+# python anneal.py --net $MODEL_NAME --dataset CIFAR10 --data_path /vulcanscratch/psando/cifar-10/ \
+# --recipe $RECIPE --eps 8 --budget 1.0 --save poison_dataset \
+# --cifar_ckpt_dir /vulcanscratch/psando/cifar_model_ckpts/ --cifar_adv_ckpt_dir /vulcanscratch/psando/cifar_model_ckpts/adv \
+# --poison_path ${POISON_DATASET_DIR}/${RECIPE}_${MODEL_NAME}_name=${ATTACKNAME}_iter=${ATTACKITER} \
+# --attackname ${ATTACKNAME} --attackiter $ATTACKITER --init zero 
 
 # Evaluate poison
-# python poison_evaluation/main.py --model_name $MODEL_NAME --epochs 100 \
-# --poison_path ${POISON_DATASET_DIR}/${RECIPE}_${MODEL_NAME}_name=${ATTACKNAME}_iter=${ATTACKITER} \
-# --cifar_path /vulcanscratch/psando/cifar-10 --disable_tqdm --workers 4
+python poison_evaluation/main.py --model_name $MODEL_NAME --epochs 100 \
+--poison_path ${POISON_DATASET_DIR}/${RECIPE}_${MODEL_NAME}_name=${ATTACKNAME}_iter=${ATTACKITER} \
+--cifar_path /vulcanscratch/psando/cifar-10 --disable_tqdm --workers 4 --lr 0.025 --use_wd --use_scheduler
 
 # Evaluate transferability
-# python poison_evaluation/eval_transferability.py ${POISON_DATASET_DIR}/${RECIPE}_${MODEL_NAME}_name=${ATTACKNAME}_iter=${ATTACKITER} --disable_tqdm --workers 4
+python poison_evaluation/eval_transferability.py ${POISON_DATASET_DIR}/${RECIPE}_${MODEL_NAME}_name=${ATTACKNAME}_iter=${ATTACKITER} --disable_tqdm --workers 4 
