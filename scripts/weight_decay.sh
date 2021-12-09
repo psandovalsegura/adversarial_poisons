35c1b4344b18c6ec9c587925d0d7a7456fec72c5
Pedro Sandoval <psandovalsegura@gmail.com>
2021-12-09 11:50:21 -0500

    weight decay experiment using pgd poison

--- scripts/weight_decay.sh
+++ scripts/weight_decay.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --account=djacobs
-#SBATCH --job-name=weight-4
+#SBATCH --job-name=weight-exp-pgd-2
 #SBATCH --time=3-00:00:00
 #SBATCH --partition=dpart
 #SBATCH --qos=medium
@@ -20,7 +20,7 @@ export SCRIPT_DIR="/cfarhomes/psando/Documents/adversarial_poisons"
 
 export POISON_DATASET_DIR='/vulcanscratch/psando/untrainable_datasets/adv_poisons/fresh_craft'
 export MODEL_NAME='ResNet18'
-export EPS='4'
+# export EPS='4'
 
 # Set environment 
 mkdir $WORK_DIR
@@ -32,12 +32,13 @@ pip3 install -r ${SCRIPT_DIR}/requirements.txt
 # Use class-wise poisons
 # 8/255: classwise_random_eps=8
 # 64/255: classwise_random_eps=64
+# 2/255 PGD-250 poison 
 declare -a WD_FLOATS=('0' '1e-4' '2e-4' '3e-4' '4e-4' '5e-4' '6e-4')
 for WD_FLOAT in ${WD_FLOATS[@]}; do
 
 # Evaluate poison
 python poison_evaluation/main.py --model_name $MODEL_NAME --epochs 100 \
---poison_path ${POISON_DATASET_DIR}/classwise_random_eps=8 \
+--poison_path ${POISON_DATASET_DIR}/targeted_ResNet18_iter=250_eps=2 \
 --cifar_path /vulcanscratch/psando/cifar-10 --disable_tqdm --workers 4 --lr 0.025 --use_scheduler --weight_decay ${WD_FLOAT}
 
 done
